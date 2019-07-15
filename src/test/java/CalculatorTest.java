0d8f28fc958178a3bf094f03b65133cc90bf3f4a
Adamsh6 <adam.s.houten@gmail.com>
2019-07-15 14:07:58 +0100

    finished calculator, created water bottle

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -14,7 +14,22 @@ public class CalculatorTest {
 
     @Test
     public void canAddNumbers(){
-        assertEquals()
+        assertEquals(21.5, calculator.add(20.4, 1.1), 0);
+    }
+
+    @Test
+    public void canSubtractNumbers(){
+        assertEquals(14.3, calculator.subtract(21.6, 7.3), 0);
+    }
+
+    @Test
+    public void canMultiplyNumbers(){
+        assertEquals(2.5, calculator.multiply(1.25, 2.0), 0);
+    }
+
+    @Test
+    public void canDivideNumbers(){
+        assertEquals(2.0, calculator.divide(6.4, 3.2), 0);
     }
 
 }
