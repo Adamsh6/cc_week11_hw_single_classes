c196f19f3da184760e4e9031a6e5641e316436bd
Adamsh6 <adam.s.houten@gmail.com>
2019-07-15 13:59:12 +0100

    set up calculator test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAddNumbers(){
+        assertEquals()
+    }
+
+}
